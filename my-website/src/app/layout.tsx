8a13d3ceaab8cff7cae08ded8043166d9a470771
Matt <mkbernier@gmail.com>
2025-05-05 14:48:29 -0600

    Fixed to match jsx requirements

--- my-website/src/app/layout.tsx
+++ my-website/src/app/layout.tsx
@@ -25,15 +25,18 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <!-- Google tag (gtag.js) -->
+        {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-EHZ8F1GBXV"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-EHZ8F1GBXV');
-        </script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-EHZ8F1GBXV');
+            `,
+          }}
+        />
         <script
           dangerouslySetInnerHTML={{
             __html: `
